0a7895ccd8303f40a60fae749aaccd869e27831a
code-designphil <phil.pieper@studium.uni-hamburg.de>
2025-03-16 19:47:16 +0100

    feat: finalizes routing

--- services/Router.js
+++ services/Router.js
@@ -7,12 +7,45 @@ const Router = {
         Router.go(route);
       });
     });
+    // Event handler for URL changes
+    window.addEventListener("popstate", function (event) {
+      Router.go(event.state.route, false);
+    });
+
     // Check the initial URL
     Router.go(location.pathname);
   },
   go: function (route, addToHistory = true) {
     if (addToHistory) {
       history.pushState({ route }, null, route);
+    }
+
+    let pageElement = null;
+
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Your Order";
+        break;
+      default:
+        if (route.startsWith("/product/")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Details";
+          const paramId = route.substring(route.lastIndexOf("/") + 1);
+          pageElement.dataset.id = paramId;
+        }
+    }
+
+    if (pageElement) {
+      const mainElement = document.querySelector("main");
+      mainElement.innerHTML = "";
+      mainElement.appendChild(pageElement);
+      window.screenX = 0;
+      window.screenY = 0;
     }
   },
 };
